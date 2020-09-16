8c7c2108067f532780fbeee377a976f4e73f3254
OsEstrada <00064318@uca.edu.sv>
2020-09-15 22:11:47 -0600

    version 1

---
+++ RSA/Encrypt.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace RSA
+{
+    public static class Encrypt
+    {
+        public static RSAParameters publicKey;
+        public static RSAParameters privateKey;
+
+        public static void GenerateKeys()
+        {
+            using(var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                publicKey = rsa.ExportParameters(false);
+                privateKey = rsa.ExportParameters(true);
+
+            }
+        }
+
+        public static byte[] EncryptMessage(byte[] message)
+        {
+            using(var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(publicKey);
+                return rsa.Encrypt(message, false);
+            }
+        }
+
+        public static byte[] DecryptMessage(byte[] encryptMessage)
+        {
+            using(var rsa = new RSACryptoServiceProvider(2048))
+            {
+                rsa.PersistKeyInCsp = false;
+                rsa.ImportParameters(privateKey);
+                return rsa.Decrypt(encryptMessage, false);
+            }
+        }
+    }
+}
---
+++ RSA/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Text;
+
+namespace RSA
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            string msg = "Hello world";
+            Encrypt.GenerateKeys();
+
+            //Convierte el mensaje de tipo string a una cadena de bytes utilizando la codificacion UTF_8
+            var encoded = Encoding.Unicode.GetBytes(msg);
+            var encrypted = Encrypt.EncryptMessage(encoded);
+            var decrypted = Encrypt.DecryptMessage(encrypted);
+
+            Console.WriteLine("Original: " + msg);
+            Console.WriteLine("Encriptado " + BitConverter.ToString(encrypted));
+            Console.WriteLine("Desencriptado " + Encoding.Unicode.GetString(decrypted));
+        }
+    }
+}
